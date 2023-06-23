dd292a59085370849d07719f182f21a54d1197cf
akimolka <akimooolga@yandex.ru>
2023-06-24 00:12:11 +0300

    Solution corrected

--- main.cpp
+++ main.cpp
@@ -1,97 +1,112 @@
-#include "bigint.hpp"
+// #include "bigint.hpp"
 
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <cmath>
 
 // number types  ------
 
 using BigInteger = long long;
 using Rational = long double;
 
-bool IsRatioSmallerThenOnePlusEpsilon(BigInteger numerator, BigInteger denominator, const Rational& epsilon) {
-    // return numerator / denominator < (1 + epsilon);
-    return numerator < denominator * (1 + epsilon);
-}
-
 // number types  ------
 
+struct DpState {
+    BigInteger min_weight;
+    BigInteger cost;
+};
 
 struct Item {
     BigInteger weight;
     BigInteger cost;
+    size_t reduced_cost; // used as index in dp
 };
 
-using State = Item;
-
-bool operator<(const State& lhs, const State& rhs) {
-    // if weight is equal, state with greater cost goes first
-    return std::tie(lhs.weight, rhs.cost) < std::tie(rhs.weight, lhs.cost);
+void update_state(DpState& dest_state, const DpState& source_state, const Item& item) {
+    if (dest_state.min_weight == -1 || dest_state.min_weight > source_state.min_weight + item.weight) {
+        dest_state.min_weight = source_state.min_weight + item.weight;
+        dest_state.cost = source_state.cost + item.cost;
+    }
 }
 
+void resize_for_update(std::vector<DpState>& dp, const BigInteger& max_weight, const Item& item) {
+    size_t max_reduced_cost = dp.size();
+    for (size_t i = 0; i < dp.size(); ++i)
+        if (dp[i].min_weight != -1 && dp[i].min_weight + item.weight <= max_weight)
+            max_reduced_cost = std::max(max_reduced_cost, i + item.reduced_cost + 1);
+    dp.resize(max_reduced_cost, DpState{-1, 0});
+}
 
-// gets non-empty ordered set of states, adds new item to each, merges sets
-void update(std::vector<State>& states, const Item& new_item) {
-    std::vector<State> new_states = states;
-    for (int i = 0; i < new_states.size(); ++i) {
-        new_states[i].weight += new_item.weight;
-        new_states[i].cost += new_item.cost;
-    }
-
-    std::vector<State> all_states(2 * states.size());
-    std::merge(states.begin(), states.end(),
-                new_states.begin(), new_states.end(),
-                all_states.begin());
-
-    // remove useless states
-    int iter = 0;
-    for (int i = 1; i < all_states.size(); ++i) {
-        if (all_states[i].cost > all_states[iter].cost) {
-            all_states[++iter] = all_states[i];
+void update_states(std::vector<DpState>& dp, const BigInteger& max_weight, const Item& item) {
+    long long old_size = dp.size();
+    resize_for_update(dp, max_weight, item);
+    for (long long i = old_size - 1; i >= 0; --i) {
+        if (dp[i].min_weight != -1 && dp[i].min_weight + item.weight <= max_weight) {
+            update_state(dp[i + item.reduced_cost], dp[i], item);
         }
     }
-    all_states.resize(iter + 1);
-
-    std::swap(states, all_states);
 }
 
+// O(n^3/eps) time & O(n^2/eps) memory
+BigInteger solve_reduced_problem(
+            const BigInteger& max_weight,
+            const std::vector<Item>& items) {
 
-// removes all states from the end with weight bigger than max_weight
-void crop(std::vector<State>& states, BigInteger max_weight) {
-    while (states.back().weight > max_weight) {
-        states.pop_back();
+    std::vector<DpState> dp_least_weight = {DpState{0, 0}};
+
+    for (const auto& item : items) {
+        update_states(dp_least_weight, max_weight, item);
     }
+
+    BigInteger best_cost = 0;
+    for (const auto& state : dp_least_weight) {
+        if (state.min_weight != -1)
+            best_cost = std::max(best_cost, state.cost);
+    }
+    return best_cost;
 }
 
-// if there are 3 consecutive states A, B, C, such that cost of C <= (1 + eps) cost of A, B is removed
-void sift(std::vector<State>& states, const Rational& precision) {
-    if (states.size() <= 2) return;
-    int index_a = 0;
-    for (int index_c = 2; index_c < states.size(); ++index_c) {
-        if (IsRatioSmallerThenOnePlusEpsilon(states[index_c].cost, states[index_a].cost, precision)) {
-            states[index_a + 1] = states[index_c];
-        } else {
-            states[index_a + 2] = states[index_c];
-            index_a += 1;
-        }
+void reduce_cost(
+            std::vector<Item>& items,
+            const Rational& precision) {
+
+    // reduced_cost = floor(cost * n / (precision * max_cost))
+    size_t n = items.size();
+    BigInteger max_cost = 0;
+    for (const auto& item : items) {
+        max_cost = std::max(max_cost, item.cost);
     }
-    states.resize(index_a + 2);
+
+    Rational reduction_coef;
+    if (precision == 0 || max_cost == 0 || n / (precision * max_cost) >= 1)  // Reduction won't help
+        reduction_coef = 1;
+    else
+        reduction_coef = n / (precision * max_cost);
+
+    for (auto& item : items) {
+        item.reduced_cost = std::floor(item.cost * reduction_coef);
+    }
 }
 
 BigInteger get_approximate_optimal_cost(
-            int n,
-            BigInteger max_weight,
+            const BigInteger& max_weight,
             const Rational& precision,
-            const std::vector<Item>& items) {
-    std::vector<State> reachable_states;
-    reachable_states.push_back(Item{BigInteger(0), BigInteger(0)});
+            const std::vector<BigInteger>& weights,
+            const std::vector<BigInteger>& costs) {
 
-    for (int i = 0; i < n; ++i) {
-        update(reachable_states, items[i]);
-        crop(reachable_states, max_weight);
-        sift(reachable_states, precision);
+    // copy data
+    size_t n = weights.size();
+    std::vector<Item> items(n);
+    for (int i = 0; i < n; i++) {
+        items[i].weight = weights[i];
+        items[i].cost = costs[i];
     }
-    return reachable_states.back().cost;
+
+    // calc reduced cost
+    reduce_cost(items, precision);
+
+    return solve_reduced_problem(max_weight, items);
 }
 
 int main() {
@@ -99,15 +114,16 @@ int main() {
     BigInteger max_weight;
     std::cin >> n >> max_weight;
 
+    std::vector<BigInteger> weights(n);
+    std::vector<BigInteger> costs(n);
+    for (int i = 0; i < n; ++i) {
+        std::cin >> weights[i] >> costs[i];
+    }
+
     Rational precision;
     std::cin >> precision;
 
-    std::vector<Item> items(n);
-    for (int i = 0; i < n; ++i) {
-        std::cin >> items[i].weight >> items[i].cost;
-    }
-
-    std::cout << get_approximate_optimal_cost(n, max_weight, precision, items) << '\n';
+    std::cout << get_approximate_optimal_cost(max_weight, precision, weights, costs) << '\n';
 
     return 0;
 }
---
+++ standard.cpp
@@ -0,0 +1,3 @@
+//
+// Created by Olga Akimova on 22.06.2023.
+//
